dd5e2fecec69028fee0cbd560286ad44b1e36c4d
Juan <juan.parra@smartsoft.com.co>
2023-04-30 15:58:11 -0500

    Proyecto de Recetas de bebidas terminada

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Container } from "react-bootstrap";
+import Formulario from "./components/Formulario";
+import { CategoriasProvider } from "./context/CategoriasProvider";
+import { BebidasProvider } from "./context/BebidasProvider";
+import ListadoBebidas from "./components/ListadoBebidas";
+import ModalBebida from "./components/ModalBebida";
+
+const App = () => {
+  return (
+    <CategoriasProvider>
+      <BebidasProvider>
+        <header className="py-5">
+          <h1>Buscador de bebidas</h1>
+        </header>
+
+        <Container className="mt-5">
+          <Formulario />
+          <ListadoBebidas />
+          <ModalBebida />
+        </Container>
+      </BebidasProvider>
+    </CategoriasProvider>
+  )
+};
+
+export default App;
